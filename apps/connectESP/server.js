70f30aeaf711d0eb33bc8cf00019a8bc603f2684
sinaschnebelt <75085397+sinaschnebelt@users.noreply.github.com>
2023-02-01 15:48:11 +0100

    Create server.js

---
+++ apps/connectESP/server.js
@@ -0,0 +1,18 @@
+NRF.setServices({
+  '9d020001-bf5f-1d1a-b52a-fe52091d5b12': {
+      '9d020001-bf5f-1d1a-b52a-fe52091d5b25': {
+        description: 'Bangle distance meassuarement',
+        notify: true,
+        readable: true,
+        value: "distance test",
+        writable: true,
+      }
+      },
+      '9d020001-bf5f-1d1a-b52a-fe52091d5b26': {
+        description: 'Bangle hrv meassuerement',
+        notify: true,
+        readable: true,
+        value: "hrv test"
+      }
+
+});
